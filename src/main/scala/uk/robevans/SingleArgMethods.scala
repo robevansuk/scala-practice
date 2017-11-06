ff9ff128d25f54fd49737526b097ac66cf94f88e
Rob Evans <robert.evans@springernature.com>
2017-11-06 08:48:26 +0000

    adding single arg method example

---
+++ src/main/scala/uk/robevans/SingleArgMethods.scala
@@ -0,0 +1,21 @@
+package uk.robevans
+
+object SingleArgMethods extends App {
+
+  val greetings : Array[String] = new Array[String](10)
+
+  greetings(0) = "Hello World"
+  greetings(1) = "Hi World"
+  greetings(2) = "Hey World"
+  greetings(3) = "Howdy World"
+  greetings(4) = "G'day World"
+  greetings(5) = "Bonjour World"
+  greetings(6) = "Guten tag World"
+  greetings(7) = "Shalom World"
+  greetings(8) = "Ciao World"
+  greetings(9) = "Hola World"
+
+
+  for (i <- 0 to 9) // 0.to(9) <- single arg method needs no dot notation or parentheses
+    println(greetings(i))
+}
