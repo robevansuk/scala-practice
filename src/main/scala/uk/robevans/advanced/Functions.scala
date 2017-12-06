ff315688afc1be985be229269970e27a1639ad95
Rob Evans <rwevans1984@gmail.com>
2017-12-06 14:24:31 +0000

    adding some function examples and a new package ready for next section

--- src/main/scala/uk/robevans/advanced/Functions.scala
+++ src/main/scala/uk/robevans/advanced/Functions.scala
@@ -34,13 +34,33 @@ object Functions extends App {
 
   makeATable1(5, 5, "x")
 
+  println("----------")
+
+
 
   def makeATable2 = (x:Int, y:Int, someCharacter: String)  => {
+    val fillCell = (someChar:String) => print(someChar)
     val makeRow =
-      (y: Array[String], someChar:String) => println(y.mkString(someChar))
+      (y: Array[String], someChar:String) => { y.foreach(fillCell); println }
     val table = Array.fill(x,y)(someCharacter)
     table.foreach(makeRow(_, someCharacter))
   }
 
   makeATable2(5, 5, "x")
+
+  // using underscores as placeholders for whatever args are expected/implied
+  def sum1 (a: Int, b:Int, c:Int) = a + b + c
+  val a1 = sum1 _ // a partially applied function with 0 args supplied. a1.apply expects 3 args.
+  println(s"a1(1,2,3) -> ${a1(1, 2, 3)} should = 6")
+  println(s"a1.apply(2,3,4) -> ${a1.apply(2, 3, 4)} should = 9")
+
+
+  // using underscores as placeholders for whatever args are expected/implied
+  def sum2 (a: Int, b:Int, c:Int) = a + b + c
+  val a2 = sum2 (1, _:Int , 0) // another partially applied function with 2 args supplied and 1 missing. a2.apply expects 1 arg.
+  println(s"a2(1) -> ${a2(1)} should = 2")
+  println(s"a2.apply(3) -> ${a2.apply(3)} should = 4")
+
+
+
 }
---
+++ src/main/scala/uk/robevans/advanced/ParameterExamples.scala
@@ -0,0 +1,44 @@
+package uk.robevans.advanced
+
+class ParameterExamples {
+
+  // Named param out: and a default = Console.out
+  def printTime(out: java.io.PrintStream = Console.out) =
+    out.println("time = " + System.currentTimeMillis())
+
+  def isGoodEnough(guess: Double): Boolean = {
+    if (guess < 1) true
+    else false
+  }
+
+  def improve(guess: Double): Double = {
+    guess - 1
+  }
+
+  // tail recursion avoids vars, is more compact and as efficient/fast as a while loop so is preferable.
+  // The Scala compiler detects tail recursion and replaces it with a jump back to the beginning of the function
+  def estimate(guess: Double):Double = {
+    if (isGoodEnough(guess)) guess
+    else improve(guess)
+  }
+
+  // Example of how to lazily evaluate some argument supplied as a parameter.
+  def assertSomething(predicate: => Boolean) = {
+    if (true && !predicate) {
+      throw IllegalArgumentException
+    }
+  }
+
+  // Non-lazy evaluation of somethingElse (will blow up regardless because predicate will be eagerly evaluated)
+  def assertSomethingElse(predicate: Boolean) = {
+    if (true && !predicate) {
+      throw IllegalArgumentException
+    }
+  }
+
+  assertSomething(5>3) // not evaluated until !predicate is called on
+  assertSomethingElse(5>3) // evaluated before method is called
+
+
+
+}
