5f7357488988104939edf4dad174c6d1ebfa9e39
Rob Evans <rwevans1984@gmail.com>
2018-08-03 10:09:36 +0100

    improved formatting

--- src/main/scala/uk/robevans/http4s/Server.scala
+++ src/main/scala/uk/robevans/http4s/Server.scala
@@ -1,4 +1,3 @@
-
 import cats.effect.{Effect, IO}
 import fs2.StreamApp
 import org.http4s.HttpService
@@ -16,20 +15,27 @@ object Server extends StreamApp[IO] {
 
   val logger = LoggerFactory.getLogger(getClass)
 
-  val serverConfig: Config = Config (
+  val serverConfig: Config = Config(
     Port(8080)
   )
 
-  def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = ServerStream.streamp[IO]
+  def stream(args: List[String],
+             requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] =
+    ServerStream.streamp[IO]
 }
 
 object ServerStream {
-   import Server._
+  import Server._
 
-  def serverRoutes[F[_]: Effect]: HttpService[F] = new ServerRoutes[F](serverConfig).service
+  def serverRoutes[F[_]: Effect]: HttpService[F] =
+    new ServerRoutes[F](serverConfig).service
 
-  def streamp[F[_]: Effect](implicit ec: ExecutionContext): fs2.Stream[F, StreamApp.ExitCode] = {
+  def streamp[F[_]: Effect](
+      implicit ec: ExecutionContext): fs2.Stream[F, StreamApp.ExitCode] = {
     logger.info(s"Server started on port: ${serverConfig.port.value}")
-    BlazeBuilder[F].bindHttp(serverConfig.port.value, "0.0.0.0").mountService(serverRoutes, "/").serve
+    BlazeBuilder[F]
+      .bindHttp(serverConfig.port.value, "0.0.0.0")
+      .mountService(serverRoutes, "/")
+      .serve
   }
 }
