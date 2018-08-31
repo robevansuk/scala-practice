ab146b1579ec5a45c9eb02d7d2a83afcc40118c1
Rob Evans <rwevans1984@gmail.com>
2018-08-31 14:05:39 +0100

    add test for flatMap on OptionCatsMonads

--- src/test/scala/uk/robevans/scalawithcats/chp4/CatsMonadsTest.scala
+++ src/test/scala/uk/robevans/scalawithcats/chp4/CatsMonadsTest.scala
@@ -4,11 +4,18 @@ import org.scalatest.{FunSpec, Matchers}
 
 class CatsMonadsTest extends FunSpec with Matchers {
 
-  describe("Pure Monad") {
-    it ("Wraps a value in a Cats monad") {
+  describe("OptionCatsMonad") {
+    it ("Wraps a value in a Cats monad for pure function") {
       val optionCatsMonads = new OptionCatsMonads
       val result = optionCatsMonads.pure(22)
       result should be (Some(22))
+    }
+
+    it("flatMaps over the OptionCatsMonad") {
+      val optionCatsMonads = new OptionCatsMonads
+      val input = Option(22)
+      val result = optionCatsMonads.flatMap[Int, String](input, (i => Some(s"$i!!!")))
+      result should be (Option("22!!!"))
     }
   }
 
